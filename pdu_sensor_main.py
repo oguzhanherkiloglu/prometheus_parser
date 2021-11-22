0bbaac923eca035d205c01268577f345ce5b388b
oguzhanherkiloglu <oguz@bitnet.com.tr>
2021-11-22 23:18:39 +0300

    apcrpdu

---
+++ pdu_sensor_main.py
@@ -0,0 +1,72 @@
+import datetime
+
+import requests
+
+import settings_humidity_sensor as settings
+
+import collections
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def get_data_from_prometheus():
+    for prometheus_instance in settings.PROMETHEUS_INSTANCES:
+        url = 'http://' + settings.PROMETHEUS_URL_AND_PORT + '/api/v1/query_range?query={instance=\"' + prometheus_instance + '\"}&start=' + settings.PROMETHEUS_START + '&end=' + settings.PROMETHEUS_END + '&step=' + settings.PROMETHEUS_STEP
+        print(url)
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        response = requests.request("GET", url, headers=headers)
+        if response is not None:
+            if response.status_code == 200:
+                response_as_json = response.json()
+                json_response, file_path = convert_json_to_proper_json_array(
+                    response_as_json["data"]["result"])
+                text_file = open(file_path, "w")
+                write_data_to_txt(text_file, str(prometheus_instance) + " SERVER VALUES")
+                logger.info(str(prometheus_instance) + " SERVER VALUES")
+                result_dict = dict()
+                for object_key, object_value in json_response.items():
+                    result_dict[convert_epoch_time_to_datetime(object_key)] = object_value
+                    write_data_to_txt(text_file, str(result_dict))
+                    logger.info(result_dict)
+                    logger.info('\n')
+                    result_dict.clear()
+                close_txt_file(text_file)
+
+
+def convert_json_to_proper_json_array(json_response):
+    object_dict = collections.defaultdict(list)
+    file_path = settings.PROMETHEUS_TXT_FILE_PATH
+    for object in json_response:
+        if (object['metric'].get('__name__') is not None) and (
+                object['metric'].get('__name__') == 'rPDUIdentDevicePowerWatts'):
+            if object['metric'].get('__name__') == 'rPDUIdentDevicePowerWatts':
+                for object_values in object['values']:
+                    object_dict[object_values[0]].append({
+                        "Device Load Watts": object_values[1]
+                    })
+
+    return object_dict, file_path
+
+
+def convert_epoch_time_to_datetime(epoch_time):
+    datetime_time = datetime.datetime.fromtimestamp(epoch_time).strftime('%Y-%m-%d %H:%M:%S')
+    return str(datetime_time)
+
+
+def write_data_to_txt(text_file, string):
+    text_file.write(string + "\n")
+    print(string)
+    print('\n')
+
+
+def close_txt_file(text_file):
+    text_file.close()
+
+
+if __name__ == '__main__':
+    get_data_from_prometheus()
---
+++ settings_pdu_sensor.py
@@ -0,0 +1,22 @@
+PROMETHEUS_INSTANCES = ['192.168.105.209']
+PROMETHEUS_STEP = '10s'
+PROMETHEUS_START = '2021-11-20T10:30:00.000Z'
+PROMETHEUS_END = '2021-11-20T10:32:20.999Z'
+PROMETHEUS_URL_AND_PORT = '192.168.105.98:9090'
+PROMETHEUS_TXT_FILE_PATH = 'C:/Users/oguzhan.herkiloglu/Desktop/test/apcrpdu_RackCabinet3_power.txt'
+HUMIDITY_SENSOR_LABELS = {
+    '1054205155': 'Humidity (1)',
+    '1121716336': 'Humidity (Cabin 3 - Back - 33U)',
+    '1668196833': 'Humidity (Cabin 3 - Back - 33U)',
+    '1744856019': 'Humidity (Cabin 3 - Front - 26U)',
+    '1974269701': 'Humidity (2)',
+    '206918816': 'Humidity (1)',
+    '2804425567': 'Humidity (Cabin 1 - Front - 26U)',
+    '3273299739': 'Humidity (Cabin 4 - Front - 26U)',
+    '338404919': 'Humidity (Cabin 1 - Back - 26U)',
+    '380332237': 'Humidity (1)',
+    '4181308384': 'Humidity (Cabin 2 - Front - 26U)',
+    '4244400748': 'Humidity (1)',
+    '581338442': 'Humidity (Cabin 3 - Back - 13U)',
+    '976244450': 'Humidity (Cabin 2 - Back - 26U)',
+}
